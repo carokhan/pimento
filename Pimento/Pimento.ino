db74ab0e1d1b3f8196d4eca856bc12fc1c88b8ab
carokhan <apoorva.saur7@gmail.com>
2022-09-24 08:03:05 -0400

    clean code but not commented

--- Pimento/Pimento.ino
+++ Pimento/Pimento.ino
@@ -1,11 +1,12 @@
 #include <Servo.h>
 
-int smaxFLpwm = 3;
-int smaxFRpwm = 6; // 6
-int smaxBLpwm = 5; // 5
-int smaxBRpwm = 9;
-double fl0, fr1, bl2, br3;
-int fl0C, fr1C, bl2C, br3C, fl0C2, bl2C2;
+int neoFrontLeft = 3;
+int neoFrontRight = 6;
+int neoBackLeft = 5;
+int neoBackRight = 9;
+double frontLeft0, frontRight1, backLeft2, backRight3;
+int frontLeft0scaled, frontRight1scaled, backLeft2scaled, backRight3scaled,
+    frontLeft0scaledRev, backLeft2scaledRev;
 
 int leftXChannel = 4;
 int leftYChannel = 3;
@@ -13,39 +14,20 @@ int rightXChannel = 1;
 
 int separator = 5000;
 
-Servo smaxs[4];
+Servo neos[4];
 
 int ppmPin = 2;
 int channelAmount = 6;
 
-unsigned long int current, past, interval;
-int order[15], channel1, channel[6], channelInit[15];
-
 long int kx, ky, kstrafe;
 
-unsigned long int a, b, c;
-int x[15], ch1[15], ch[7], i;
+unsigned long int current, past, interval;
+int peaks[15], frame[15], channel[7], channelNum;
 
 void mecanum() {
-  kx = ch[leftXChannel];
-  ky = ch[leftYChannel];
-  kstrafe = ch[rightXChannel];
-
-  // smaxs[0].write(ky + kx + kstrafe);
-  // smaxs[1].write(ky - kx - kstrafe);
-  // smaxs[2].write(ky - kx + kstrafe);
-  // smaxs[3].write(ky + kx - kstrafe);
-  // Serial.print("\t");Serial.print("\t");Serial.print("\t");
-  // Serial.print(ky);
-  // Serial.print("\t");
-  // Serial.print(kx);
-  // Serial.print("\t");
-  // Serial.print(kstrafe);
-  // Serial.print("\t");
-  // Serial.print("\t");
-  // Serial.print("\t");
-  // Serial.print("\t");
-  // Serial.println();
+  kx = channel[leftXChannel];
+  ky = channel[leftYChannel];
+  kstrafe = channel[rightXChannel];
 
   if ((ky > 1100) || (ky < -100)) {
     Serial.println("ky");
@@ -70,107 +52,77 @@ void mecanum() {
     kstrafe = 500;
   }
 
-  // Serial.println("Hi");  
+  frontLeft0 = ky + kx + kstrafe;   // 0 to 3000
+  frontRight1 = ky - kx - kstrafe;  // -2000 to 1000
+  backLeft2 = ky - kx + kstrafe;    // -1000 to 2000
+  backRight3 = ky + kx - kstrafe;   // -1000 to 2000
 
-  fl0 = ky + kx + kstrafe; // 0 to 3000
-  fr1 = ky - kx - kstrafe; // -2000 to 1000
-  bl2 = ky - kx + kstrafe; // -1000 to 2000
-  br3 = ky + kx - kstrafe; // -1000 to 2000
+  frontLeft0scaled = (int)((double)frontLeft0 / 3.0 + 1000);
+  frontLeft0scaledRev = ((frontLeft0scaled - 1500) * -1) + 1500;
+  frontRight1scaled = (int)(((double)frontRight1 + 2000) / 3.0 + 1000);
+  backLeft2scaled = (int)((((double)backLeft2 + 1000) / 3.0) + 1000);
+  backLeft2scaledRev = ((backLeft2scaled - 1500) * -1) + 1500;
+  backRight3scaled = (int)(((double)backRight3 + 1000) / 3.0 + 1000);
 
-  fl0C = (int)((double) fl0 / 3.0 + 1000);
-  fl0C2 = ((fl0C - 1500) * -1) + 1500;
-  fr1C = (int)(((double) fr1 + 2000) / 3.0 + 1000);
-  // fr1C = abs(fr1C - 1500) * -1 + fr1C;
-
-  // bl2C = (int)(((((double) bl2 + 1000) / 3.0) - 500) * 1.05 + 1500);
-  bl2C = (int)((((double) bl2 + 1000) / 3.0) + 1000);
-  bl2C2 = ((bl2C - 1500) * -1) + 1500;
-  br3C = (int)(((double) br3 + 1000) / 3.0 + 1000);
-  // br3C = abs(br3C - 1500) * -1 + br3C;
-  // Serial.print(kx);Serial.print("\t\t");Serial.print(ky);Serial.print("\t\t");Serial.print(kstrafe);
-  // Serial.print(fl0); Serial.print("\t\t"); Serial.print(fl0C); Serial.print("\t\t");
-
-  // Serial.println("Test");
-
-  Serial.print(fl0C2);
+  Serial.print(frontLeft0scaledRev);
   Serial.print("\t");
-  Serial.print(fr1C);
+  Serial.print(frontRight1scaled);
   Serial.print("\t");
-  Serial.print(bl2C2);
+  Serial.print(backLeft2scaledRev);
   Serial.print("\t");
-  Serial.print(br3C);
+  Serial.print(backRight3scaled);
   Serial.print("\t");
   Serial.println();
 
-  smaxs[0].write(fl0C2);
-  smaxs[1].write(fr1C);
-  smaxs[2].write(bl2C2);
-  smaxs[3].write(br3C);
+  neos[0].write(frontLeft0scaledRev);
+  neos[1].write(frontRight1scaled);
+  neos[2].write(backLeft2scaledRev);
+  neos[3].write(backRight3scaled);
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(smaxFLpwm, OUTPUT);
-  pinMode(smaxFRpwm, OUTPUT);
-  pinMode(smaxBLpwm, OUTPUT);
-  pinMode(smaxBRpwm, OUTPUT);
-  smaxs[0].attach(smaxFLpwm);
-  smaxs[1].attach(smaxFRpwm);
-  smaxs[2].attach(smaxBLpwm);
-  smaxs[3].attach(smaxBRpwm);
+  pinMode(neoFrontLeft, OUTPUT);
+  pinMode(neoFrontRight, OUTPUT);
+  pinMode(neoBackLeft, OUTPUT);
+  pinMode(neoBackRight, OUTPUT);
+  neos[0].attach(neoFrontLeft);
+  neos[1].attach(neoFrontRight);
+  neos[2].attach(neoBackLeft);
+  neos[3].attach(neoBackRight);
 
   pinMode(ppmPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), read_me, FALLING);
-  // ESC1.attach(3);
-  // ESC2.attach(5);
-  // ESC3.attach(6);
-  // ESC4.attach(9,1000,2000); // (pin min pulse width max pulse width) 
+  attachInterrupt(digitalPinToInterrupt(2), ppmSetup, FALLING);
 }
 
 void loop() {
-  read_rc();
-
-  // Serial.print(ch[2]);Serial.print("\t");
-  // Serial.print(ch[leftYChannel]);Serial.print("\t");
-  // Serial.print(ch[leftXChannel]);Serial.print("\t");
-  // Serial.print(ch[rightXChannel]);Serial.print("\t");
-
-  // Serial.print(ch[5]);Serial.print("\t");
-  // Serial.print(ch[6]);Serial.print("\t");
-  // Serial.println();
-  /*ESC1.write();
-  ESC2.write();
-  ESC3.write();
-  ESC4.write();
-  */
+  ppmRead();
   mecanum();
   delay(20);
 }
 
-void read_me() {
-  //this code reads value from RC reciever from PPM pin (Pin 2 or 3)
-  //this code gives channel values from 0-1000 values 
-  //    -: ABHILASH :-    //
-  a = micros(); //store time value a when pin value falling
-  c = a - b; //calculating time inbetween two peaks
-  b = a; // 
-  x[i] = c; //storing 15 value in array
-  i = i + 1;
-  if (i == 15) {
-    for (int j = 0; j < 15; j++) {
-      ch1[j] = x[j];
+void ppmSetup() {
+  current = micros();
+  interval = current - past;
+  past = current;
+  peaks[channelNum] = interval;
+  channelNum = channelNum + 1;
+  if (channelNum == 15) {
+    for (int i = 0; i < 15; i++) {
+      frame[i] = peaks[i];
     }
-    i = 0;
+    channelNum = 0;
   }
-} //copy store all values from temporary array another array after 15 reading  
-void read_rc() {
-  int i, j, k = 0;
-  for (k = 14; k > -1; k--) {
-    if (ch1[k] > separator) {
-      j = k;
+}
+
+void ppmRead() {
+  int i, activeChannel, ppmChannel = 0;
+  for (ppmChannel = 14; ppmChannel > -1; ppmChannel--) {
+    if (frame[ppmChannel] > separator) {
+      activeChannel = ppmChannel;
     }
-  } //detecting separation space 10000us in that another array                     
-  for (i = 1; i <= 6; i++) {
-    ch[i] = (ch1[i + j] - 1000);
   }
-} //assign 6 channel values after separation space
+  for (i = 1; i <= channelAmount; i++) {
+    channel[i] = (frame[i + activeChannel] - 1000);
+  }
+}
